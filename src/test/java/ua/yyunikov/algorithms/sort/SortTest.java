3179bf226e5cff5f8b726301545181cd00e24d77
yyunikov <yyunikov@gmail.com>
2015-06-16 00:41:18 +0300

    Added few tests for sorting

--- src/test/java/ua/yyunikov/algorithms/sort/SortTest.java
+++ src/test/java/ua/yyunikov/algorithms/sort/SortTest.java
@@ -1,6 +1,7 @@
 package ua.yyunikov.algorithms.sort;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -8,14 +9,37 @@ import java.util.Random;
 
 public class SortTest {
 
+    private static final int MILLION = 1000000;
+
     private static final int[] UNSORTED_UNREPEATABLE_ARRAY = new int[]{5, 7, 1, 3, 9, 15, 4, 55, 12, 0};
     private static final int[] SORTED_UNREPEATABLE_ARRAY = new int[]{0, 1, 3, 4, 5, 7, 9, 12, 15, 55};
 
     private static final int[] UNSORTED_REPEATABLE_ARRAY = new int[]{5, 7, 1, 3, 9, 15, 4, 3, 55, 12, 0, 1, 55};
     private static final int[] SORTED_REPEATABLE_ARRAY = new int[]{0, 1, 1, 3, 3, 4, 5, 7, 9, 12, 15, 55, 55};
 
-    private static final int[] UNSORTED_RANDOM_BIG_ARRAY = generateRandomBigArray();
+    private static final int[] UNSORTED_RANDOM_BIG_ARRAY = generateRandomArray(MILLION);
     private static final int[] SORTED_RANDOM_BIG_ARRAY = javaSort(UNSORTED_RANDOM_BIG_ARRAY);
+
+    private int[] unrepeatableTestArray;
+    private int[] repeatableTestArray;
+    private int[] randomBigTestArray;
+
+    @Before
+    public void initArrays() {
+        unrepeatableTestArray = UNSORTED_UNREPEATABLE_ARRAY.clone();
+        repeatableTestArray = UNSORTED_REPEATABLE_ARRAY.clone();
+        randomBigTestArray = UNSORTED_RANDOM_BIG_ARRAY.clone();
+    }
+
+    @Test
+    public void testJavaSort() {
+        Arrays.sort(unrepeatableTestArray);
+        Assert.assertArrayEquals(unrepeatableTestArray, SORTED_UNREPEATABLE_ARRAY);
+        Arrays.sort(repeatableTestArray);
+        Assert.assertArrayEquals(repeatableTestArray, SORTED_REPEATABLE_ARRAY);
+        Arrays.sort(randomBigTestArray);
+        Assert.assertArrayEquals(randomBigTestArray, SORTED_RANDOM_BIG_ARRAY);
+    }
 
     @Test
     public void testMergeSort() {
@@ -24,29 +48,30 @@ public class SortTest {
 
     @Test
     public void testInsertionSort() {
-        testSort(new InsertionSort());
+        testSlowSort(new InsertionSort());
     }
 
     @Test
-    public void testSelectionSort() {
-        testSort(new SelectionSort());
+    public void testSlowSelectionSort() {
+        testSlowSort(new SelectionSort());
     }
 
-    // TODO add performance test
+    private void testSlowSort(final Sort sort) {
+        Assert.assertArrayEquals(sort.sort(unrepeatableTestArray), SORTED_UNREPEATABLE_ARRAY);
+        Assert.assertArrayEquals(sort.sort(repeatableTestArray), SORTED_REPEATABLE_ARRAY);
+    }
 
     private void testSort(final Sort sort) {
-        Assert.assertArrayEquals(sort.sort(UNSORTED_UNREPEATABLE_ARRAY), SORTED_UNREPEATABLE_ARRAY);
-        Assert.assertArrayEquals(sort.sort(UNSORTED_REPEATABLE_ARRAY), SORTED_REPEATABLE_ARRAY);
-        Assert.assertArrayEquals(sort.sort(UNSORTED_RANDOM_BIG_ARRAY), SORTED_RANDOM_BIG_ARRAY);
+        Assert.assertArrayEquals(sort.sort(unrepeatableTestArray), SORTED_UNREPEATABLE_ARRAY);
+        Assert.assertArrayEquals(sort.sort(repeatableTestArray), SORTED_REPEATABLE_ARRAY);
+        Assert.assertArrayEquals(sort.sort(randomBigTestArray), SORTED_RANDOM_BIG_ARRAY);
     }
 
-    private static int[] generateRandomBigArray() {
-        final int hunredThousand = 100000;
-
-        final int[] bigArray = new int[hunredThousand];
+    private static int[] generateRandomArray(final int size) {
+        final int[] bigArray = new int[size];
         final Random random = new Random();
 
-        for(int i = 0; i < hunredThousand; i++) {
+        for(int i = 0; i < size; i++) {
             bigArray[i] = random.nextInt();
         }
 
