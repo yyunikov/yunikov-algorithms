fc235228d0714cbe8c3a09bf998a5f3f8e1b08b7
yyunikov <yyunikov@gmail.com>
2015-06-21 21:53:12 +0300

    Implemented randomized quick sort

--- src/main/java/ua/yyunikov/algorithms/sort/QuickSort.java
+++ src/main/java/ua/yyunikov/algorithms/sort/QuickSort.java
@@ -4,7 +4,13 @@ import ua.yyunikov.algorithms.util.ArrayUtils;
 
 import java.util.Random;
 
+/**
+ * <a href="https://en.wikipedia.org/?title=Quicksort">Quick sort</a> algorithm with randomized pivot point.
+ * Running time is n*log(n).
+ */
 public class QuickSort extends Sort {
+
+    private final Random random = new Random(System.currentTimeMillis());
 
     @Override
     protected void doSort(final int[] array) {
@@ -18,33 +24,32 @@ public class QuickSort extends Sort {
      * @param endPoint highest bound to choose the pivot from
      * @return pivot index between start point and end point
      */
-    protected int choosePivotIndex(final int startPoint, final int endPoint) {
+    protected int choosePivotIndex(final int startPoint, final int endPoint)  {
         if (startPoint == endPoint) {
             return startPoint;
         }
 
-        final Random random = new Random(System.currentTimeMillis());
         return random.nextInt(endPoint - startPoint) + startPoint;
     }
 
     private void quickSort(final int[] array, final int leftIndex, final int rightIndex) {
         if (leftIndex < rightIndex) {
             final int pivotIndex = choosePivotIndex(leftIndex, rightIndex - 1);
-            final int newPivotIndex = partition(array, pivotIndex, array.length);
+            final int newPivotIndex = partition(array, pivotIndex, leftIndex, rightIndex);
 
             quickSort(array, leftIndex, newPivotIndex);
             quickSort(array, newPivotIndex + 1, rightIndex);
         }
     }
 
-    private int partition(final int[] array, final int pivotIndex, final int rangeIndex) {
-        // place the pivot at first position
-        ArrayUtils.swap(array, 0, pivotIndex);
-        final int pivot = array[0];
+    private int partition(final int[] array, final int pivotIndex, final int leftIndex, final int rightIndex) {
+        // put the pivot to the first position of part to be sorted
+        ArrayUtils.swap(array, leftIndex, pivotIndex);
+        final int pivot = array[leftIndex];
 
-        int partitionedMiddle = 1; // holds the middle point between partitioned elements
+        int partitionedMiddle = leftIndex + 1; // holds the middle point between partitioned elements
         // left from partitionedMiddle are less than the pivot, right from partitionedMiddle are bigger than the pivot
-        for (int currentPosition = 1; currentPosition < rangeIndex; currentPosition++) {
+        for (int currentPosition = leftIndex + 1; currentPosition < rightIndex; currentPosition++) {
             // swap only if element is less than the pivot
             // otherwise it stays on the right part (where the bigger elements than the pivot are)
             if (array[currentPosition] < pivot) {
@@ -55,7 +60,7 @@ public class QuickSort extends Sort {
 
         // place the pivot at the partitioned middle
         // so elements less than the pivot are left from it, and the ones bigger than the pivot are right from it
-        ArrayUtils.swap(array, 0, partitionedMiddle - 1);
+        ArrayUtils.swap(array, leftIndex, partitionedMiddle - 1);
         return partitionedMiddle - 1;
     }
 }
