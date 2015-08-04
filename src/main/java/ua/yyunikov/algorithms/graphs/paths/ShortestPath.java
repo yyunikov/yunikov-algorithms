e2118ea46ff776110c6cdefbe87e5f7647a6ba09
yyunikov <yyunikov@gmail.com>
2015-08-04 22:44:50 +0300

    Removed unused code

--- src/main/java/ua/yyunikov/algorithms/graphs/paths/ShortestPath.java
+++ src/main/java/ua/yyunikov/algorithms/graphs/paths/ShortestPath.java
@@ -22,15 +22,5 @@ public abstract class ShortestPath {
         return doFind(graph, from, to);
     }
 
-    /**
-     * Counts the number of minimum cuts in the graph.
-     *
-     * @param graphArray graph representation in two dimensional array
-     * @return minimum cuts count
-     */
-    public int findShortestPath(final int[][] graphArray, final int from, final int to) {
-        return doFind(Graph.createGraph(graphArray), new Vertex(from), new Vertex(to));
-    }
-
     protected abstract int doFind(final Graph graph, final Vertex from, final Vertex to);
 }
